31d5156996780edd421488de5c60c0914d7d0095
Hai <wordpree.dev@gmail.com>
2017-12-28 10:11:42 +1000

    added gulp-jslint gulp-concat gulp-uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,38 @@
 var gulp       = require('gulp');
 var gulpSass   = require('gulp-compass');
+var gulpJslint   = require('gulp-jslint');
 var gulpUglify = require('gulp-uglify');
 var gulpCat    = require('gulp-concat');
 var gulpRename = require('gulp-rename');
 var gulpCss    = require('gulp-clean-css');
 gulp.task('sass',function(){
-	return gulp.src('preprocess/style.scss')
+	return gulp.src('source/scss/style.scss')
 	       .pipe(gulpSass({
-	       	 css: 'build/development',
-	       	 sass: 'preprocess',
+	       	 css: 'build/development/css',
+	       	 sass: 'source/scss',
 	       	 style: 'expanded'
 	       }))
 	       .pipe(gulpCss())
 	       .pipe(gulpRename('style.min.css'))
-	       .pipe(gulp.dest('build/development'))
+	       .pipe(gulp.dest('build/development/css'))
+});
+
+gulp.task('jsLint',function(){
+	return gulp.src('source/js/**.js')
+	    .pipe(gulpJslint())
+	    .pipe(gulpJslint.reporter('default'))
+});
+
+gulp.task('js',function(){
+	return gulp.src('source/js/**.js')
+	   .pipe(gulpCat('main.js'))
+	   .pipe(gulpUglify())
+	   .pipe(gulp.dest('build/development/js'))
 });
 
 gulp.task('watch',function(){
-	 gulp.watch('preprocess/*.scss',['sass']);
+	 gulp.watch(['source/style/**.scss'],['sass']);
+	 gulp.watch(['source/js/**.js'],['jsLint','js']);
 });
 gulp.task('default',['watch']);
 
